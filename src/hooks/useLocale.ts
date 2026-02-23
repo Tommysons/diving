7688ecb492af76424d8a4982661a0c6157507ea9
Tommysons <lokatorist2@inbox.lv>
2026-02-23 09:27:45 -0800

    refactor landing page into components

--- src/hooks/useLocale.ts
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { usePathname } from 'next/navigation'
-
-export default function useLocale() {
-  const pathname = usePathname() || '/'
-  return pathname.startsWith('/ru') ? 'ru' : 'en'
-}
